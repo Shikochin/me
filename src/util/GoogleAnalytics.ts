ad7810b1ce42be6a409eeb22ed963c8726dfbe49
ケリティアル <keritial@gmail.com>
2023-02-12 14:06:53 +0800

    Update GoogleAnalytics.ts

--- src/util/GoogleAnalytics.ts
+++ src/util/GoogleAnalytics.ts
@@ -2,7 +2,7 @@ import { config } from "src/environments/config";
 
 export function enableGoogleAnalytics(): void {
 	document.head.append(
-		`<script defer src="https://www.googletagmanager.com/gtag/js?id=${config.googleAnalytics.id}"></script>`,
+		`<script defer src="https://www.googletagmanager.com/gtag/js?id=${config.analytics.google.id}"></script>`,
 		`<script>
 			globalThis.dataLayer ??= [];
 			globalThis.gtag = (...args) => {
